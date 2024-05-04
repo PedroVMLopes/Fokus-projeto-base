9a0e8008851dafcbbf7a11a43d4bb5977fc918ee
pedrovmlopes <pedrovitormlopes@gmail.com>
2024-05-04 19:35:25 -0300

    Alteração das imagens e textos pelos botões

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const timer = document.querySelector(".app__card-timer");
 
 const image = document.querySelector(".app__image");
 
+const text = document.querySelector(".app__title");
+
 // Variáveis dos botões do site
 const btnFocus = document.querySelector(".app__card-button--foco");
 const btnShort = document.querySelector(".app__card-button--curto");
@@ -30,4 +32,27 @@ btnLong.addEventListener("click", () => {
 function alterarContexto(contexto) {
   html.setAttribute("data-contexto", contexto);
   image.setAttribute("src", `/imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      text.innerHTML = `
+            Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `;
+      break;
+    case "descanso-curto":
+      text.innerHTML = `
+            Que tal dar uma respirada?<br>
+            <strong class="app__title-strong">Faça uma pausa curta.</strong>
+            `;
+      break;
+    case "descanso-longo":
+      text.innerHTML = `
+            Hora de voltar a superfície<br>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `;
+      console.log("descanso longo");
+      break;
+    default:
+      break;
+  }
 }
