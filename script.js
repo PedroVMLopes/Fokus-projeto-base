039d8d37ebb5d5d40f549ebc3cf2c7fa42de8af7
pedrovmlopes <pedrovitormlopes@gmail.com>
2024-05-04 17:35:58 -0300

    Atualizacao do projeto

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const html = document.querySelector("html");
 
-const btnFoco = document.querySelector(".app__card-button--foco");
+const timer = document.querySelector(".app__card-timer");
 
-const btnCurto = document.querySelector(".app__card-button--curto");
+const image = document.querySelector(".app__image");
 
-const btnLongo = document.querySelector(".app__card-button--longo");
+// Variáveis dos botões do site
+const btnFocus = document.querySelector(".app__card-button--foco");
+const btnShort = document.querySelector(".app__card-button--curto");
+const btnLong = document.querySelector(".app__card-button--longo");
+const btnPlay = document.querySelector("#start-pause");
 
-btnFoco.addEventListener("click", () => {
+// Tempo dos tipos de temporizador
+const timerFocus = 1500;
+const timerShort = 300;
+const timerLong = 900;
+
+btnFocus.addEventListener("click", () => {
   html.setAttribute("data-contexto", "foco");
+  image.setAttribute("src", "/imagens/foco.png");
 });
 
-btnCurto.addEventListener("click", () => {
+btnShort.addEventListener("click", () => {
   html.setAttribute("data-contexto", "descanso-curto");
+  image.setAttribute("src", "/imagens/descanso-curto.png");
 });
 
-btnLongo.addEventListener("click", () => {
+btnLong.addEventListener("click", () => {
   html.setAttribute("data-contexto", "descanso-longo");
+  image.setAttribute("src", "/imagens/descanso-longo.png");
 });
