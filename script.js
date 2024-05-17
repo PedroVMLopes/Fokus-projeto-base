4e67f189573cc196cdcdc9e0f58ee4c5a38d0afb
pedrovmlopes <pedrovitormlopes@gmail.com>
2024-05-17 18:24:40 -0300

    Funções para captar data e hora atual

--- script.js
+++ script.js
@@ -136,3 +136,14 @@ function showTime() {
 
 // Invoca a função globalmente para o timer estar sempre aparecendo na tela
 showTime();
+
+// Funções para captar a data e hora atual do usuário
+const currentDate = new Date();
+
+const ano = currentDate.getFullYear(); // Acessa o ano
+const mes = currentDate.getMonth(); // Acessa o mês - Janeiro é 0, Fevereiro é 1, ..., Dezembro é 11
+const dia = currentDate.getDate(); // Acessa o dia
+const horas = currentDate.getHours(); // Acessa as horas
+const minutos = currentDate.getMinutes(); // Acessa os minutos
+const segundos = currentDate.getSeconds(); // Acessa os segundos
+const milissegundos = currentDate.getMilliseconds(); // Acessa os milissegundos
